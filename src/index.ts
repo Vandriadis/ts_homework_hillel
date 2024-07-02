cfee70f6627972f7628c19ffba8245dd29d45107
Vandriadis <fobbiee@protonmail.com>
2024-07-02 10:27:39 +0300

    gg

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
+// 1
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K];
+};
 
+// 2
+type DeepRequireReadonly<T> = {
+  readonly [K in keyof T]-?: T[K] extends object ? DeepRequireReadonly<T[K]> : T[K];
+};
+
+// 3
+type UpperCaseKeys<T> = {
+  [K in keyof T as Uppercase<K & string>]: T[K];
+};
+
+// 4
+type PropertysDescriptor<T> = {
+  configurable?: boolean;
+  enumerable?: boolean;
+  writable?: boolean;
+  value?: T;
+  get?(): T;
+  set?(value: T): void;
+};
+
+type ObjectToPropertyDescriptor<T> = {
+  [K in keyof T]: PropertysDescriptor<T[K]>;
+};
